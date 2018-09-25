7e7b5a818b8912e1b6987ae5879f4800402ea208
msaggio <34450701+msaggio@users.noreply.github.com>
2018-09-25 11:51:32 -0400

    Add files via upload

---
+++ class Snake.js
@@ -0,0 +1,45 @@
+class Snake{
+  constructor (){
+    this.position_ = 0;
+    this.direction_=0;
+  }
+  move(squares){
+    if (this.direction_== 0)
+      this.position_=this.position_ + squares;
+    else this.position_=this.position_ -squares;
+   
+  }
+  turn(){
+    if (this.direction_==0)
+     this.direction_=1;
+    else this.direction_=0;
+  }
+
+  get position(){
+    return this.position_;
+  }
+
+  turnLeft(){
+    if (this.position)
+
+  }
+}
+
+class Point{
+  constructor(){
+    this.x_=20;
+    this.y_=20;
+  }
+
+  get position(){
+    return this.position_;
+  }
+}
+
+let mainSnake = new Snake("main");
+mainSnake.move(10);
+console.log(mainSnake.position)
+mainSnake.turn()
+mainSnake.move(20)
+console.log(mainSnake.position)
+
